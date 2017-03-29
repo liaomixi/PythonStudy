b40694e3e68edaeeb8ba765dbb97e608ea6e618b
lmxair@126.com <lmxair@126.com>
2017-03-29 14:17:06 +0800

    test

--- WenJianCaoZuo/WenJianDaKai.py
+++ WenJianCaoZuo/WenJianDaKai.py
@@ -3,9 +3,18 @@
 # f = open("test01", "r")
 # f = open("test01", "w") # 覆盖写
 # f = open("test01", "a") # 追加写
+# f = open("test01", "ab") # 追加写，以字节的方式
 # f = open("test01", "x") # 文件存在就报错，不存在就创建并写文件，python3里面新加功能
 
-f = open("test01", "x")
+## 以字节方式读写文件
+f = open("test01", "ab")
+f.write(bytes("liaomixi", encoding="utf8"))
+f.close()
+
+f = open("test01", "rb")
+data = f.read()
+print(str(data, encoding="utf8"))
+f.close()
 
 ############################
 # 操作文件
