41516efb752a751e96eacfb6ec214bc3d82be63f
lmxair@126.com <lmxair@126.com>
2017-04-10 10:10:04 +0800

    test

--- ZhuangShiQi/ZhuangShiQi.py
+++ ZhuangShiQi/ZhuangShiQi.py
@@ -7,7 +7,7 @@ def outer(fun):       # fun就等于show
 # @ + 函数，装饰器
 # 功能：
 # 1。自动执行outer函数并且将其下面的函数名show当作参数传递，相当于 执行 outer(show)
-# 2。将outer函数的返回值，重新赋值给show，这里就是 show = inner，相当show函数变成了inner
+# 2。将outer函数的返回值，重新赋值给show，这里就是 show = inner，相当show函数变成了inner，后面再调用show函数就是调用inner函数了
 
 @outer
 def show():
