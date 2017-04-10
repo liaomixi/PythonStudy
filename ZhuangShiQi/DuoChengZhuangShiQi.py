5eb0ced6c49cc2250948b01540e5d2e496186cf4
lmxair@126.com <lmxair@126.com>
2017-04-10 17:00:01 +0800

    test

--- ZhuangShiQi/DuoChengZhuangShiQi.py
+++ ZhuangShiQi/DuoChengZhuangShiQi.py
@@ -1,25 +1,40 @@
 LOGIN = 0
 ADMIN = 0
 
-def check_login():
-    pass
+def check_login(func):
+    def inner(*args, **kwargs):
+
+        res = func(*args, **kwargs)
+        return res
+
+
+    return inner
+
 
 def check_admin():
     pass
 
 
 def login():
-    pass
+    print("登录成功！")
 
 def admin():
-    pass
+    print("欢迎管理员")
 
 
 def main():
-    inp = input("1: login; 2: admin >>>> ")
-    if inp == '1':
-        login()
-    elif inp == '2':
-        admin()
+    global LOGIN
+    global ADMIN
+
+    while True:
+        inp = input("1: login; 2: admin >>>> ")
+        if inp == "1":
+            LOGIN = 1
+            login()
+        elif inp == "2":
+            LOGIN = 1
+            ADMIN = 1
+            admin()
+
 
 main()
