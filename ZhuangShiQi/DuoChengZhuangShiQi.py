294d1ac188cce27c9e960a09f6fae68d35eea982
lmxair@126.com <lmxair@126.com>
2017-04-10 15:06:13 +0800

    test

---
+++ ZhuangShiQi/DuoChengZhuangShiQi.py
@@ -0,0 +1,25 @@
+LOGIN = 0
+ADMIN = 0
+
+def check_login():
+    pass
+
+def check_admin():
+    pass
+
+
+def login():
+    pass
+
+def admin():
+    pass
+
+
+def main():
+    inp = input("1: login; 2: admin >>>> ")
+    if inp == '1':
+        login()
+    elif inp == '2':
+        admin()
+
+main()
--- ZhuangShiQi/ZhuangShiQi.py
+++ ZhuangShiQi/ZhuangShiQi.py
@@ -2,7 +2,6 @@ def outer(fun):       # fun就等于 show 或 test
     def inner(*args, **kwargs):
         print("before")
         ret = fun(*args, **kwargs)         # 这里就是执行 show() 函数内的语句，并处理原函数返回值
-        print("after")
         return ret
 
     return inner
