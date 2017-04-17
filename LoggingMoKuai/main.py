e3d6024c060cc551a408bf591b5f5be0c2af2fd0
lmxair@126.com <lmxair@126.com>
2017-04-17 13:46:21 +0800

    test

--- LoggingMoKuai/main.py
+++ LoggingMoKuai/main.py
@@ -1,24 +1,17 @@
 import logging
 
-
-#  只记录 WARNING 以及比 WARNING 更严重的级别的日志, 非常普通的用法
-# logging.basicConfig(format="%(asctime)s  %(message)s", datefmt="%m/%d/%Y %I:%M:%S", filename="test.log", level=logging.WARNING)
-# logging.warning("[warning] liao mi xi")
-# logging.critical("[critical] liao mi xi")
-# logging.error("[error] liao mi xi")
-
+# 日志级别严重级（依次递减）：CRITICAL  ERROR   WARNING   INFO   DEBUG
 
 # 同时把log日志打到标准输出和文件里
 
 # 首先创建logger对象, 这个logger对象最好是一个全局变量，整个程序都可以用
 LOGGER = logging.getLogger("TEST-LOG")
-LOGGER.setLevel(logging.WARNING)
 
 # 创建handler1对象，主要处理输出到标准输出(屏幕)
 handler1 = logging.StreamHandler()
 handler1.setLevel(logging.WARNING)
 
-# 创建handler2对象，主要处理输出到文件
+# 创建handler2对象，主要处理输出到文件。只记录error和error级别以上的日志
 handler2 = logging.FileHandler("test.log")
 handler2.setLevel(logging.ERROR)
 
@@ -43,5 +36,6 @@ def mLog(type, message):
     elif type == "debug":
         LOGGER.debug(message)
 
+mLog("warning", "liao mi xi")
 
 
