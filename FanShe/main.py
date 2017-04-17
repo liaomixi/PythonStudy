68452a3f9981188d3027251ea36974f22cf62267
lmxair@126.com <lmxair@126.com>
2017-04-17 14:43:56 +0800

    test

--- FanShe/main.py
+++ FanShe/main.py
@@ -1,26 +1,26 @@
-# import FanShe.s1
+import FanShe.lib.s1
 #
 # FanShe.s1.f1()
 #
 # # 通过反射方式调用s1中的f1()
 # # hasattr（检查） getattr（查找） setattr delattr  这就是几个反射
 #
-# str = "f1"
-# if hasattr(FanShe.s1, str):
-#     func = getattr(FanShe.s1, str) # 反射就是利用字符串的形式去对象中操作（查找／检查／删除／设置）相同名字的成员
-#     func()
-# else:
-#     print("no func {:s}".format(str))
+str = "f1"
+if hasattr(FanShe.lib.s1, str):
+    func = getattr(FanShe.lib.s1, str) # 反射就是利用字符串的形式去对象中操作（查找／检查／删除／设置）相同名字的成员
+    func()
+else:
+    print("no func {:s}".format(str))
 
 
 ########################################################################################################################
 ########################################################################################################################
 
 # 通过字符串导入指定模块
-mn = "s1"
+mn = "FanShe.lib.s1"
 fn = "f1"
 
-obj = __import__(mn)            # 使用字符串导入模块
+obj = __import__(mn, fromlist=True)            # 使用字符串导入模块, fromlist 说明你路径写的是什么，我就导入什么，不写就导入失败
 if hasattr(obj, fn):
     func = getattr(obj, fn)
     func()
