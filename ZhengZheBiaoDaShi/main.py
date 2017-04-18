86ce66d0bffa41d1dda75e0f9b4c02f64430cb38
lmxair@126.com <lmxair@126.com>
2017-04-18 15:12:59 +0800

    test

--- ZhengZheBiaoDaShi/main.py
+++ ZhengZheBiaoDaShi/main.py
@@ -6,6 +6,7 @@ import re
 # .{1,5} 匹配 1 到 5 个字符
 # .{4} 匹配 4 个字符
 # . 匹配任意 1 个字符
+# \d 匹配任何十进制数字   \D 匹配任何非数字字符   \s  匹配任何空白字符    \w 匹配任何数字字母组合
 
 li = re.findall(" liao ", "liaomixi is good liaomixixxxx liao ")
 print(li, type(li))
