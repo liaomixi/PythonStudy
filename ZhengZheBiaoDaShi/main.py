64f6237b25df45496484203733736c1175f22dc7
lmxair@126.com <lmxair@126.com>
2017-04-18 15:00:50 +0800

    test

--- ZhengZheBiaoDaShi/main.py
+++ ZhengZheBiaoDaShi/main.py
@@ -2,7 +2,10 @@ import re
 
 # .* 匹配 0 到多个字符
 # .+ 匹配 1 到多个字符
-# 
+# .? 匹配 0 或 1 个字符
+# .{1,5} 匹配 1 到 5 个字符
+# .{4} 匹配 4 个字符
+# . 匹配任意 1 个字符
 
 li = re.findall(" liao ", "liaomixi is good liaomixixxxx liao ")
 print(li, type(li))
