ee2e9de71582dfceddca5ee869f924a70e63e216
lmxair@126.com <lmxair@126.com>
2017-04-20 10:46:03 +0800

    test

--- LeiHeDuiXiang/main.py
+++ LeiHeDuiXiang/main.py
@@ -1,13 +1,21 @@
-from LeiHeDuiXiang import c1
-
-obj = c1.Bar()
-obj.show()
+# from LeiHeDuiXiang import c1
+#
+# obj = c1.Bar()
+# obj.show()
 
 
 class Test:
-    def show(self):
-        print(self.name)
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-o1 = Test()
-o1.name = "liaomixi"
+    def show(self):
+        print(self.name, self.age)
+
+o1 = Test("liaomixi", "18")
 o1.show()
+
+print()
+
+o2 = Test("mixi", "20")
+o2.show()
