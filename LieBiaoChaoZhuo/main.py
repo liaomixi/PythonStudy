360c4c449c9920cf9f716a3f9bd88d2e00064c50
lmxair@126.com <lmxair@126.com>
2017-04-20 11:18:16 +0800

    test

--- LieBiaoChaoZhuo/main.py
+++ LieBiaoChaoZhuo/main.py
@@ -1,5 +1,5 @@
 # 列表切片操作
-# li[ start : end : step ]  step默认步长是1，start索引从0开始, 不包括end位置的值
+# li[ start : end : step ]  step默认步长是1(step为负数的时候，li中的值就从倒着输出)，start索引从0开始, 不包括end位置的值
 # 或
 # li[ start : end ]
 
@@ -12,6 +12,11 @@ print(li[0:2])
 
 print(li[0:-2])   # 这里位置0就是元素 1 ， 位置-2就是元素 4，其实相当于 li[0:3], 因为位置3和-2上的元素都是4
 
+print(li[-4:-2])  # 等价于 li[1:3]
+
+print(li[-4:-1:2])
+
+print(li[::-2])
 
 
 
