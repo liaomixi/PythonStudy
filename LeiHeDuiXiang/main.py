dddd08b9c9a012a20b96d4fd5eb5c080f43321d7
lmxair@126.com <lmxair@126.com>
2017-04-20 16:46:37 +0800

    test

--- LeiHeDuiXiang/main.py
+++ LeiHeDuiXiang/main.py
@@ -16,14 +16,20 @@ class Test:
 # 继承
 class Te(Test):
     def __init__(self):
-        super(Te, self).__init__("liaomixi", 18)   # 调用父类构造方法
+        super(Te, self).__init__("liaomixi", 18)   # 调用父类构造方法, 或 Test.__init__(self, "liaomixi", 18)
 
+    # 重写父类中的 show 方法
     def show(self):
         super(Te, self).show()   # 执行父类的show方法。或 Test.show(self)
-
         print("Te show")
+
+    def test(self):
+        print("Te test")
+
 
 
 o1 = Te()
 o1.show()
+
+
 
