a76d94ba1209ced14d82a5466e20ca1b50f775f8
liaomixi <lmxair@126.com>
2020-06-28 18:08:45 +0800

    modif 1111

---
+++ LieBiaoCaoZuo/QiePian.py
@@ -0,0 +1,16 @@
+lst = ["java", "c++", "python"]
+
+print(lst[-3:-1])
+print(lst[:-1])
+print(lst[-3:])
+
+print(lst[1:4])
+
+# list[start: end: step]
+lst = [1, 3, 5, 6, 7, 8]
+print(lst[::2])
+
+lst = [1, 2, 3]
+#列表反转 [1, 2, 3] 变为 [3, 2, 1]
+print(lst[::-1])
+
